e43b22e5e9eafea617a9784a200cb61baad7085b
korket <korket1234@gmail.com>
2022-11-21 18:17:05 +0700

    feat: add gif to the abilities

--- pages/class/assassin/script.js
+++ pages/class/assassin/script.js
@@ -1,22 +1,29 @@
 const ability_d = document.querySelector('.ability-d');
+const gif_d = document.querySelector('.abilities-gif-d');
 const button_d = document.querySelector('.button-d');
 
 const ability_q = document.querySelector('.ability-q');
+const gif_q = document.querySelector('.abilities-gif-q');
 const button_q = document.querySelector('.button-q');
 
 const ability_w = document.querySelector('.ability-w');
+const gif_w = document.querySelector('.abilities-gif-w');
 const button_w = document.querySelector('.button-w');
 
 const ability_e = document.querySelector('.ability-e');
+const gif_e = document.querySelector('.abilities-gif-e');
 const button_e = document.querySelector('.button-e');
 
 const ability_r = document.querySelector('.ability-r');
+const gif_r = document.querySelector('.abilities-gif-r');
 const button_r = document.querySelector('.button-r');
 
 const ability_t = document.querySelector('.ability-t');
+const gif_t = document.querySelector('.abilities-gif-t');
 const button_t = document.querySelector('.button-t');
 
 const ability_f = document.querySelector('.ability-f');
+const gif_f = document.querySelector('.abilities-gif-f');
 const button_f = document.querySelector('.button-f');
 
 button_d.addEventListener('click', showD);
@@ -35,6 +42,13 @@ function showD() {
     ability_r.style.display = 'none';
     ability_t.style.display = 'none';
     ability_f.style.display = 'none';
+    gif_d.style.display = 'flex';
+    gif_q.style.display = 'none';
+    gif_w.style.display = 'none';
+    gif_e.style.display = 'none';
+    gif_r.style.display = 'none';
+    gif_t.style.display = 'none';
+    gif_f.style.display = 'none';
 }
 
 function showQ() {
@@ -45,6 +59,13 @@ function showQ() {
     ability_r.style.display = 'none';
     ability_t.style.display = 'none';
     ability_f.style.display = 'none';
+    gif_d.style.display = 'none';
+    gif_q.style.display = 'flex';
+    gif_w.style.display = 'none';
+    gif_e.style.display = 'none';
+    gif_r.style.display = 'none';
+    gif_t.style.display = 'none';
+    gif_f.style.display = 'none';
 }
 
 function showW() {
@@ -55,6 +76,13 @@ function showW() {
     ability_r.style.display = 'none';
     ability_t.style.display = 'none';
     ability_f.style.display = 'none';
+    gif_d.style.display = 'none';
+    gif_q.style.display = 'none';
+    gif_w.style.display = 'flex';
+    gif_e.style.display = 'none';
+    gif_r.style.display = 'none';
+    gif_t.style.display = 'none';
+    gif_f.style.display = 'none';
 }
 
 function showE() {
@@ -65,6 +93,13 @@ function showE() {
     ability_r.style.display = 'none';
     ability_t.style.display = 'none';
     ability_f.style.display = 'none';
+    gif_d.style.display = 'none';
+    gif_q.style.display = 'none';
+    gif_w.style.display = 'none';
+    gif_e.style.display = 'flex';
+    gif_r.style.display = 'none';
+    gif_t.style.display = 'none';
+    gif_f.style.display = 'none';
 }
 
 function showR() {
@@ -75,6 +110,13 @@ function showR() {
     ability_r.style.display = 'flex';
     ability_t.style.display = 'none';
     ability_f.style.display = 'none';
+    gif_d.style.display = 'none';
+    gif_q.style.display = 'none';
+    gif_w.style.display = 'none';
+    gif_e.style.display = 'none';
+    gif_r.style.display = 'flex';
+    gif_t.style.display = 'none';
+    gif_f.style.display = 'none';
 }
 
 function showT() {
@@ -85,6 +127,13 @@ function showT() {
     ability_r.style.display = 'none';
     ability_t.style.display = 'flex';
     ability_f.style.display = 'none';
+    gif_d.style.display = 'none';
+    gif_q.style.display = 'none';
+    gif_w.style.display = 'none';
+    gif_e.style.display = 'none';
+    gif_r.style.display = 'none';
+    gif_t.style.display = 'flex';
+    gif_f.style.display = 'none';
 }
 
 function showF() {
@@ -95,4 +144,11 @@ function showF() {
     ability_r.style.display = 'none';
     ability_t.style.display = 'none';
     ability_f.style.display = 'flex';
+    gif_d.style.display = 'none';
+    gif_q.style.display = 'none';
+    gif_w.style.display = 'none';
+    gif_e.style.display = 'none';
+    gif_r.style.display = 'none';
+    gif_t.style.display = 'none';
+    gif_f.style.display = 'flex';
 }
