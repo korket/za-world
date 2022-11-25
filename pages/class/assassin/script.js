3ff1f64a74c6938e559e8f5ea10cce5a774efe7d
korket <korket1234@gmail.com>
2022-11-25 22:50:44 +0700

    fix: reformat js

--- pages/class/assassin/script.js
+++ pages/class/assassin/script.js
@@ -26,131 +26,71 @@ const ability_f = document.querySelector('.ability-f');
 const video_f = document.querySelector('.ability-video-f');
 const button_f = document.querySelector('.button-f');
 
+button_d.addEventListener('click', hide);
 button_d.addEventListener('click', showD);
+button_q.addEventListener('click', hide);
 button_q.addEventListener('click', showQ);
+button_w.addEventListener('click', hide);
 button_w.addEventListener('click', showW);
+button_e.addEventListener('click', hide);
 button_e.addEventListener('click', showE);
+button_r.addEventListener('click', hide);
 button_r.addEventListener('click', showR);
+button_t.addEventListener('click', hide);
 button_t.addEventListener('click', showT);
+button_f.addEventListener('click', hide);
 button_f.addEventListener('click', showF);
 
 window.onload = showD;
 
-function showD() {
-    ability_d.style.display = 'flex';
+function hide() {
+    ability_d.style.display = 'none';
     ability_q.style.display = 'none';
     ability_w.style.display = 'none';
     ability_e.style.display = 'none';
     ability_r.style.display = 'none';
     ability_t.style.display = 'none';
     ability_f.style.display = 'none';
-    video_d.style.display = 'flex';
+    video_d.style.display = 'none';
     video_q.style.display = 'none';
     video_w.style.display = 'none';
     video_e.style.display = 'none';
     video_r.style.display = 'none';
     video_t.style.display = 'none';
     video_f.style.display = 'none';
+}
+
+function showD() {
+    ability_d.style.display = 'flex';
+    video_d.style.display = 'flex';
 }
 
 function showQ() {
-    ability_d.style.display = 'none';
     ability_q.style.display = 'flex';
-    ability_w.style.display = 'none';
-    ability_e.style.display = 'none';
-    ability_r.style.display = 'none';
-    ability_t.style.display = 'none';
-    ability_f.style.display = 'none';
-    video_d.style.display = 'none';
     video_q.style.display = 'flex';
-    video_w.style.display = 'none';
-    video_e.style.display = 'none';
-    video_r.style.display = 'none';
-    video_t.style.display = 'none';
-    video_f.style.display = 'none';
 }
 
 function showW() {
-    ability_d.style.display = 'none';
-    ability_q.style.display = 'none';
     ability_w.style.display = 'flex';
-    ability_e.style.display = 'none';
-    ability_r.style.display = 'none';
-    ability_t.style.display = 'none';
-    ability_f.style.display = 'none';
-    video_d.style.display = 'none';
-    video_q.style.display = 'none';
     video_w.style.display = 'flex';
-    video_e.style.display = 'none';
-    video_r.style.display = 'none';
-    video_t.style.display = 'none';
-    video_f.style.display = 'none';
 }
 
 function showE() {
-    ability_d.style.display = 'none';
-    ability_q.style.display = 'none';
-    ability_w.style.display = 'none';
     ability_e.style.display = 'flex';
-    ability_r.style.display = 'none';
-    ability_t.style.display = 'none';
-    ability_f.style.display = 'none';
-    video_d.style.display = 'none';
-    video_q.style.display = 'none';
-    video_w.style.display = 'none';
     video_e.style.display = 'flex';
-    video_r.style.display = 'none';
-    video_t.style.display = 'none';
-    video_f.style.display = 'none';
 }
 
 function showR() {
-    ability_d.style.display = 'none';
-    ability_q.style.display = 'none';
-    ability_w.style.display = 'none';
-    ability_e.style.display = 'none';
     ability_r.style.display = 'flex';
-    ability_t.style.display = 'none';
-    ability_f.style.display = 'none';
-    video_d.style.display = 'none';
-    video_q.style.display = 'none';
-    video_w.style.display = 'none';
-    video_e.style.display = 'none';
     video_r.style.display = 'flex';
-    video_t.style.display = 'none';
-    video_f.style.display = 'none';
 }
 
 function showT() {
-    ability_d.style.display = 'none';
-    ability_q.style.display = 'none';
-    ability_w.style.display = 'none';
-    ability_e.style.display = 'none';
-    ability_r.style.display = 'none';
     ability_t.style.display = 'flex';
-    ability_f.style.display = 'none';
-    video_d.style.display = 'none';
-    video_q.style.display = 'none';
-    video_w.style.display = 'none';
-    video_e.style.display = 'none';
-    video_r.style.display = 'none';
     video_t.style.display = 'flex';
-    video_f.style.display = 'none';
 }
 
 function showF() {
-    ability_d.style.display = 'none';
-    ability_q.style.display = 'none';
-    ability_w.style.display = 'none';
-    ability_e.style.display = 'none';
-    ability_r.style.display = 'none';
-    ability_t.style.display = 'none';
     ability_f.style.display = 'flex';
-    video_d.style.display = 'none';
-    video_q.style.display = 'none';
-    video_w.style.display = 'none';
-    video_e.style.display = 'none';
-    video_r.style.display = 'none';
-    video_t.style.display = 'none';
     video_f.style.display = 'flex';
 }
